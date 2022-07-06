f7d175b7f0a75281d6ba824e19448cfb24da5771
LucasB145 <99190894+LucasB145@users.noreply.github.com>
2022-07-06 11:44:00 +0200

    first commit

---
+++ databinding/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.u = Utilisateur(
+            "Brest",
+            18,
+            "Foulques",
+            "De Gustavio",
+            "5 rue des épines",
+            "foulques@bg.com"
+        )
+
+
+    }
+}
---
+++ databinding/src/main/java/com/example/databinding/Utilisateur.kt
@@ -0,0 +1,10 @@
+package com.example.databinding
+
+data class Utilisateur(
+    val ville: String,
+    val age: Int,
+    val prenom: String,
+    val nom: String,
+    val adresse: String,
+    val email: String
+)
