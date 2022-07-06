28db2d0161b353c10366c401b10537f6474da2ff
LucasB145 <99190894+LucasB145@users.noreply.github.com>
2022-07-06 17:00:40 +0200

    module navigation

---
+++ navigationtp/src/main/java/com/example/navigationtp/ButtonFragment.kt
@@ -0,0 +1,36 @@
+package com.example.navigationtp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+
+class ButtonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_button, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.buttonToTarget)
+
+        /*méthode 1 (Fonctionne partout)*//*
+        button.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_buttonFragment_to_targetFragment)
+        }*/
+
+        /*méthode 2 (Marche uniquement pour les boutons)*/
+        button.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_buttonFragment_to_targetFragment)
+        )
+    }
+}
